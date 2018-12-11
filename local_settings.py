ec862d4cd195c9a430d10d47dfb406a7267a8fd0
Nguyen Phuoc Thinh <npthinh1996@gmail.com>
2018-12-11 22:26:08 +0700

    delete local_settings

--- local_settings.py
+++
@@ -1,9 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2*#fc3wkv+j%d@fj9&#rciq3ld@n636xe#@w#-&h2270r%wl7p'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['checkseo.herokuapp.com']
-
-SECURE_SSL_REDIRECT = True
