31dddea9f11f66ecd1e6b4ffda24431051a48a5c
Nguyen Phuoc Thinh <npthinh1996@gmail.com>
2018-12-11 15:44:52 +0700

    ALLOWED_HOSTS

--- lvtn/settings.py
+++ lvtn/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2*#fc3wkv+j%d@fj9&#rciq3ld@n636xe#@w#-&h2270r%wl7p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['checkseo.herokuapp.com']
 
 
 # Application definition
